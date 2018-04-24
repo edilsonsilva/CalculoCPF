d095d03cf6ebc041d432b238b83747ddb5d4aece
Edilson <edilson_js@yahoo.com.br>
2018-04-24 12:28:38 -0300

    Fim do Cálculo CPF

--- Program.cs
+++ Program.cs
@@ -14,7 +14,34 @@ namespace CalculoCPF
            int resto=0;
            int rs1 = 0;
            int rs2 = 0;
-           
+           string cpffinal;
+
+           cpfCliente = "28591034813";
+           cpf9 = cpfCliente.Substring(0,9);
+           for(int i=0; i < cpf9.Length;i++)
+           {
+               rs1 += int.Parse(cpf9[i].ToString())*contagem1;
+               contagem1--;
+           }
+
+           resto = rs1 % 11;
+           if(resto < 2)
+            cpf10 = cpf9+0;
+           else
+            cpf10 = cpf9+(11-resto);
+
+            for(int i=0; i< cpf10.Length; i++)
+            {
+                rs2+=int.Parse(cpf10[i].ToString())*contagem2;
+                contagem2--;
+            }
+            resto = rs2 % 11;
+            if(resto < 2)
+            cpffinal = cpf10 + 0;
+            else
+            cpffinal = cpf10+(11-resto);
+
+
         }
     }
 }
